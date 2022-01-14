4fbf2015fe50771c3e287357fad6a0085b15225b
jdolph21 <97466942+jdolph21@users.noreply.github.com>
2022-01-14 10:12:46 -0600

    Update registration.php
    Added input validation and error messages using PHP

--- registration.php
+++ registration.php
@@ -1,7 +1,8 @@
 <?php 
-$username = $dob = $augieID = $gradYear= $firstName = $lastName = $phone = $city = $workPlace = $email = $password = "";
+$username = $confirmPassword = $dob = $augieID = $gradYear= $firstName = $lastName = $phone = $city = $workPlace = $email = $password = "";
+$usernameErr = $passwordErr = $dobErr = $cityErr = $emailErr = $gradYearErr = $lastNameErr = $firstNameErr = $workPlaceErr = $cityErr = $IDErr = $phoneNumErr = $formErr = $formSuccess = " ";
 
-
+$validInput = true;
 
 function test_input($data) {
   $data = trim($data);
@@ -26,6 +27,7 @@ if ($conn->connect_error) {
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $username = test_input($_POST["username"]);
   $email = test_input($_POST["email"]);
+  $email = filter_var($email, FILTER_SANITIZE_EMAIL);
   $gradYear = test_input($_POST["gradYear"]);
   $workPlace = test_input($_POST["workPlace"]);
   $city = test_input($_POST["location"]);
@@ -34,16 +36,158 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
   $lastName = test_input($_POST["lastName"]);
   $dob = test_input($_POST["dob"]);
   $augieID = test_input($_POST["ID"]);
-  $password = password_hash(test_input($_POST["password"]), PASSWORD_BCRYPT);
+  $password = test_input($_POST["password"]);
+  $confirmPassword = test_input($_POST["confirmPassword"]);
+
+
+  $sql= "SELECT username FROM users";
+
+  $query = mysqli_query($conn,$sql);
+
+  $result = $query->fetch_assoc();
+
+
+  if (empty($username)) {
+    $usernameErr = "*Username is required";
+    $validInput = False;
+  } elseif ($result != null) {
+    if (in_array($username, $result))  {
+    $usernameErr = "*Username is taken";
+    $validInput = False;
+  } }
+
+  if (empty($gradYear)) {
+    $gradYearErr = "*Graduation year is required";
+    $validInput = False;
+  } elseif (filter_var($phone, FILTER_SANITIZE_NUMBER_INT) === false) {
+    $gradYearErr = "*Graduation year should be a number";
+    $validInput = False;
+  }
+
+  if (empty($firstName)) {
+    $firstNameErr = "*First name is required";
+    $validInput = False;
+  } elseif (!(preg_match('/[\'^£$%&*()}{@#~?><!>,|=_+¬-]/', $firstName) == 0 && preg_match('/\d/', $firstName) == 0)) {
+    $firstNameErr = "*First name should only contain letters";
+  }
+
+  if (empty($lastName)) {
+    $lastNameErr = "*Last name is required";
+    $validInput = False;
+  } elseif (!(preg_match('/[\'^£$%&*()}{@#~?><!>,|=_+¬-]/', $lastName) == 0 && preg_match('/\d/', $lastName) == 0)) {
+    $lastNameErr = "*Last name should only contain letters";
+  }
+
+  if (empty($phone)) {
+    $phoneNumErr = "*Phone Number is required";
+    $validInput = False;
+  } elseif (filter_var($phone, FILTER_SANITIZE_NUMBER_INT) === false) {
+    $phoneNumErr = "*Phone number should be a number";
+  }
+
+
+  $sql= "SELECT email FROM users";
+
+  $query = mysqli_query($conn,$sql);
+
+  $result = $query->fetch_assoc();
+
+
+
+  if (empty($email)) {
+    $emailErr = "*Email is required";
+    $validInput = False;
+  } elseif (filter_var($email, FILTER_SANITIZE_EMAIL) === false) {
+    $emailErr = "*Email is not valid";
+    $validInput = False;
+  } elseif ($result != null) {
+    if (in_array($email, $result)) {
+    $emailErr = "*Email is already taken";
+    $validInput = False;
+  } }
+
+  if (empty($dob)) {
+    $dobErr = "*Date of birth is required";
+    $validInput = False;
+  }
+
+
+
+  $sql= "SELECT augieID FROM users";
+
+  $query = mysqli_query($conn,$sql);
+
+  $result = $query->fetch_assoc();
+
+
+
+  if (empty($augieID)) {
+    $IDErr = "*Augie ID is required";
+    $validInput = False;
+  } elseif (filter_var($augieID, FILTER_SANITIZE_NUMBER_INT) === false) {
+    $IDErr = "*Augie ID should be a number";
+    $validInput = False;
+  } elseif ($result != null) {
+    if (in_array($augieID, $result)) {
+    $IDErr = "*Augie ID is already registered";
+    $validInput = False;
+  } }
+
+  if (empty($workPlace)) {
+    $workPlaceErr = "*Work Place is required";
+    $validInput = False;
+  } elseif(!(preg_match('/[\^£$%&*()}{@#~?><!>,|=_+¬-]/', $workPlace) == 0)) {
+    $workPlaceErr = "*Work place shouldn't contain special characters";
+    $validInput = False;
+  }
+
+  if (empty($city)) {
+    $cityErr = "*Location is required";
+    $validInput = False;
+  } elseif(!(preg_match('/[\'^£$%&*()}{@#~?><!>,|=_+¬-]/', $city) == 0 && preg_match('/\d/', $city) == 0)) {
+    $cityErr = "*Location should only contain letters";
+    $validInput = False;
+  }
+
+  if (empty($password)) {
+    $passwordErr = "*Password is required";
+    $validInput = False;
+  } elseif (preg_match("/[A-Z]/", $password) == 1 && preg_match("/[a-z]/", $password) == 1 && preg_match("/\d/", $password) == 1 ) {
+    if (preg_match('/[\'^£$%&*()}{@#~?><!>,|=_+¬-]/', $password) == 0) {
+      $passwordErr = "*Password must contain atleast 1 uppercase letter, atleast 1 lowercase letter, atleast 1 number, and atleast 1 special character";
+      $validInput = False;
+    } elseif ($password != $confirmPassword) {
+      $validInput = False;
+      $passwordErr = "*Passwords do not match";
+    } 
+
+  } else {
+      $passwordErr = "*Password must contain atleast 1 uppercase letter, atleast 1 lowercase letter, atleast 1 number, and atleast 1 special character";
+      $validInput = False;
+  }
+
+  $password = password_hash($password, PASSWORD_BCRYPT);
+
+
+
+
+if($validInput) {
+  $sql = "INSERT INTO users (username, email, gradYear, workPlace, city, phoneNumber, firstName, lastName, dob, augieID, password) VALUES ('$username', '$email', '$gradYear' ,'$workPlace', '$city', '$phone', '$firstName', '$lastName', '$dob', '$augieID', '$password')";
+
+  if ($conn->query($sql) === FALSE) {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+  }
 }
 
-$sql = "INSERT INTO users (username, email, gradYear, workPlace, city, phoneNumber, firstName, lastName, dob, augieID, password) VALUES ('$username', '$email', '$gradYear' ,'$workPlace', '$city', '$phone', '$firstName', '$lastName', '$dob', '$augieID', '$password')";
-
-if ($conn->query($sql) === TRUE) {
-} else {
-  echo "Error: " . $sql . "<br>" . $conn->error;
-}
 $conn->close();
+
+if (!$validInput) {
+  $formErr = "Account could not be created";
+} else {
+  $formSuccess = "Account created";
+}
+
+}
 ?>
 
 <!DOCTYPE html>
@@ -54,10 +198,13 @@ $conn->close();
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <link rel="stylesheet" href="css/reg.css" />
     <script src="js/reg.js" defer></script>
-    <title>Viking Registraion Form</title>
+    <title>Viking Registration Form</title>
   </head>
   <body background= "images/old.jpg">
+    
     <form method="post" action="<?php echo htmlspecialchars($_SERVER["PHP_SELF"]); ?>" class="form">
+    <h2 id="message" style="color:red;"> <?php echo $formErr;?></h2>
+    <h2 id="message" style="color:green;"> <?php echo $formSuccess;?></h2>
       <h1 class="text-center">Registration Form</h1>
       <!-- Progress bar -->
       <div class="progressbar">
@@ -67,6 +214,7 @@ $conn->close();
           class="progress-step progress-step-active"
           data-title="Intro"
         ></div>
+        <div class="progress-step" data-title="Name"></div>
         <div class="progress-step" data-title="Contact"></div>
         <div class="progress-step" data-title="ID"></div>
         <div class="progress-step" data-title="Employment"></div>
@@ -76,11 +224,13 @@ $conn->close();
       <!-- Steps -->
       <div class="form-step form-step-active">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $usernameErr;?></span>
           <label for="username">Username</label>
           <input type="text" name="username" id="username" />
         </div>
 
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $gradYearErr;?></span>
           <label for="gradYear">Graduation Year</label>
           <input type="text" name="gradYear" id="gradYear" />
         </div>
@@ -92,11 +242,13 @@ $conn->close();
 
       <div class="form-step">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $firstNameErr;?></span>
           <label for="firstName">First Name</label>
           <input type="text" name="firstName" id="firstName" />
         </div>
 
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $lastNameErr;?></span>
           <label for="lastName">Last Name</label>
           <input type="text" name="lastName" id="lastName" />
         </div>
@@ -109,11 +261,13 @@ $conn->close();
 
       <div class="form-step">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $phoneNumErr;?></span>
           <label for="phone">Phone</label>
           <input type="text" name="phone" id="phone" />
         </div>
 
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $emailErr;?></span>
           <label for="email">Email</label>
           <input type="text" name="email" id="email" />
         </div>
@@ -128,11 +282,13 @@ $conn->close();
 
       <div class="form-step">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $dobErr;?></span>
           <label for="dob">Date of Birth</label>
           <input type="date" name="dob" id="dob" />
         </div>
 
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $IDErr;?></span>
           <label for="ID">Augie ID</label>
           <input type="number" name="ID" id="ID" />
         </div>
@@ -147,11 +303,13 @@ $conn->close();
 
       <div class="form-step">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $workPlaceErr;?></span>
           <label for="workPlace">Currently Working At:</label>
           <input type="text" name="workPlace" id="workPlace" />
         </div>
 
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $cityErr;?></span>
           <label for="location">Location</label>
           <input type="text" name="location" id="location" />
         </div>
@@ -166,6 +324,7 @@ $conn->close();
 
       <div class="form-step">
         <div class="input-group">
+        <span class="error" style="color:red;"> <?php echo $passwordErr;?></span>
           <label for="password">Password</label>
           <input type="password" name="password" id="password" />
         </div>
@@ -181,11 +340,9 @@ $conn->close();
 
         <div class="btns-group">
           <a href="#" class="btn btn-prev">Previous</a>
-          <input type="submit" value="Submit" class="btn" />
+          </form>
+          <input type="submit" value="Submit" class="btn"/>
         </div>
       </div>
-
-
-    </form>
   </body>
 </html>
