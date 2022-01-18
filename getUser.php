cc0e8189670452192a26b6089baa4268554b1dee
jdolph21 <97466942+jdolph21@users.noreply.github.com>
2022-01-18 11:07:21 -0600

    Update getUser.php

--- getUser.php
+++ getUser.php
@@ -13,22 +13,25 @@ if ($conn->connect_error) {
 
 $sql= "SELECT * FROM users WHERE firstName LIKE '%$q%' OR lastName LIKE '%$q%'";
 $result = mysqli_query($conn,$sql);
-
-echo "<table>";
-echo "<tr>";
-echo "<th> Image </th>";
-echo "<th> First Name </th>";
-echo "<th> Last Name </th>";
-echo "<th> Username </th>";
-echo "<th> Year Graduated </th>";
-echo "</tr>";
-while($row = mysqli_fetch_array($result)) {
-  echo "<tr><td>" . "<img  src='images/" . $row['username'] .".jpg' onerror='this.onerror=null; this.src=`images/default.jpg`' width=200 height=200></img>";
-  echo "</td> <td>" . $row['firstName'] . "</td> <td> ". $row['lastName'] . "</td> <td>" . $row['username'] . "</td> <td>";
-  echo $row['gradYear'] . "</td> <td>" ."<form action='displayUser.php' method='post'><input name='username' type='hidden' value='" . $row['username'] . "'></input><input type='submit' value='Go to Profile'></input></td></form></tr>";
-}
-
-echo "</table>";
+if (mysqli_num_rows($result) == 0) {
+  echo "<h1>No Users Found";
+} else {
+    echo "<table>";
+    echo "<tr>";
+    echo "<th> Image </th>";
+    echo "<th> First Name </th>";
+    echo "<th> Last Name </th>";
+    echo "<th> Username </th>";
+    echo "<th> Year Graduated </th>";
+    echo "</tr>";
+    while($row = mysqli_fetch_array($result)) {
+      echo "<tr><td>" . "<img  src='images/" . $row['username'] .".jpg' onerror='this.onerror=null; this.src=`images/default.jpg`' width=200 height=200></img>";
+      echo "</td> <td>" . $row['firstName'] . "</td> <td> ". $row['lastName'] . "</td> <td>" . $row['username'] . "</td> <td>";
+      echo $row['gradYear'] . "</td> <td>" ."<form action='displayUser.php' method='post'><input name='username' type='hidden' value='" . $row['username'] . "'></input><input type='submit' value='Go to Profile'></input></td></form></tr>";
+    }
+    echo "</table>";
+  }
+  
 mysqli_close($conn);
 ?>
 
